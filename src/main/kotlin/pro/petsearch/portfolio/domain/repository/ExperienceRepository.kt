f5b0781c3f013cef651f75c4e16eb97ffbad4484
ldh52 <yahopet79@gmail.com>
2024-10-09 14:54:09 +0900

    리포지토리 성능 개선

--- src/main/kotlin/pro/petsearch/portfolio/domain/repository/ExperienceRepository.kt
+++ src/main/kotlin/pro/petsearch/portfolio/domain/repository/ExperienceRepository.kt
@@ -1,10 +1,16 @@
 package pro.petsearch.portfolio.domain.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import pro.petsearch.portfolio.domain.entity.Experience
+import java.util.*
 
 interface ExperienceRepository : JpaRepository<Experience, Long> {
 
+    @Query("select e from Experience e left join fetch e.details where e.isActive = :isActive")
     fun findAllByIsActive(isActive: Boolean): List<Experience>
+
+    @Query("select e from Experience e left join fetch e.details where e.id = :id")
+    override fun findById(id: Long): Optional<Experience>
 
 }
--- src/main/kotlin/pro/petsearch/portfolio/domain/repository/ProjectRepository.kt
+++ src/main/kotlin/pro/petsearch/portfolio/domain/repository/ProjectRepository.kt
@@ -1,10 +1,15 @@
 package pro.petsearch.portfolio.domain.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import pro.petsearch.portfolio.domain.entity.Project
+import java.util.*
 
 interface ProjectRepository : JpaRepository<Project, Long> {
 
+    @Query("select p from Project p left join fetch p.skills s left join fetch s.skill where p.isActive = :isActive")
     fun findAllByIsActive(isActive: Boolean): List<Project>
 
+    @Query("select p from Project p left join fetch p.details where p.id = :id")
+    override fun findById(id: Long): Optional<Project>
 }
