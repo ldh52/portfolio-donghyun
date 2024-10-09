cd69e2296fcaec0c8a7f0ab8b3b3d18f1b3eab0c
ldh52 <yahopet79@gmail.com>
2024-10-09 17:01:43 +0900

    컨트롤러 개발

--- src/main/kotlin/pro/petsearch/portfolio/presentation/controller/PresentationApiController.kt
+++ src/main/kotlin/pro/petsearch/portfolio/presentation/controller/PresentationApiController.kt
@@ -3,34 +3,41 @@ package pro.petsearch.portfolio.presentation.controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import pro.petsearch.portfolio.presentation.dto.IntroductionDTO
+import pro.petsearch.portfolio.presentation.dto.LinkDTO
+import pro.petsearch.portfolio.presentation.dto.ProjectDTO
+import pro.petsearch.portfolio.presentation.dto.ResumeDTO
+import pro.petsearch.portfolio.presentation.service.PresentationService
 
 @RestController
 @RequestMapping("/api")
-class PresentationApiController {
+class PresentationApiController(
+    private val presentationService: PresentationService
+) {
 
     @GetMapping("/test")
     fun test(): String {
         return "OK"
     }
 
-//    @GetMapping("/v1/introductions")
-//    fun getIntroductions(): List<IntroductionDTO> {
-//        return presentationService.getIntroductions()
-//    }
-//
-//    @GetMapping("/v1/links")
-//    fun getLinks(): List<LinkDTO> {
-//        return presentationService.getLinks()
-//    }
-//
-//    @GetMapping("/v1/resume")
-//    fun getResume(): ResumeDTO {
-//        return presentationService.getResume()
-//    }
-//
-//    @GetMapping("/v1/projects")
-//    fun getProjects(): List<ProjectDTO> {
-//        return presentationService.getProjects()
-//    }
+    @GetMapping("/v1/introductions")
+    fun getIntroductions(): List<IntroductionDTO> {
+        return presentationService.getIntroductions()
+    }
+
+    @GetMapping("/v1/links")
+    fun getLinks(): List<LinkDTO> {
+        return presentationService.getLinks()
+    }
+
+    @GetMapping("/v1/resume")
+    fun getResume(): ResumeDTO {
+        return presentationService.getResume()
+    }
+
+    @GetMapping("/v1/projects")
+    fun getProjects(): List<ProjectDTO> {
+        return presentationService.getProjects()
+    }
 
 }
