c24a8935658e43328b97cedc1f6b1481ca041ab5
ldh52 <yahopet79@gmail.com>
2024-10-09 16:01:35 +0900

    서비스 개발

--- src/main/kotlin/pro/petsearch/portfolio/presentation/service/PresentationService.kt
+++ src/main/kotlin/pro/petsearch/portfolio/presentation/service/PresentationService.kt
@@ -1,6 +1,51 @@
 package pro.petsearch.portfolio.presentation.service
 
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import pro.petsearch.portfolio.presentation.dto.IntroductionDTO
+import pro.petsearch.portfolio.presentation.dto.LinkDTO
+import pro.petsearch.portfolio.presentation.dto.ProjectDTO
+import pro.petsearch.portfolio.presentation.dto.ResumeDTO
+import pro.petsearch.portfolio.presentation.repository.PresentationRepository
 
 @Service
-class PresentationService
+class PresentationService(
+    private val presentationRepository: PresentationRepository
+) {
+
+    @Transactional(readOnly = true)
+    fun getIntroductions(): List<IntroductionDTO> {
+        val introductions = presentationRepository.getActiveIntroductions()
+
+        return introductions.map { IntroductionDTO(it) }
+    }
+
+    @Transactional(readOnly = true)
+    fun getLinks(): List<LinkDTO> {
+        val links = presentationRepository.getActiveLinks()
+
+        return links.map { LinkDTO(it) }
+    }
+
+    @Transactional(readOnly = true)
+    fun getResume(): ResumeDTO {
+
+        val experiences = presentationRepository.getActiveExperiences()
+        val achievements = presentationRepository.getActiveAchievements()
+        val skills = presentationRepository.getActiveSkills()
+
+        return ResumeDTO(
+            experiences = experiences,
+            achievements = achievements,
+            skills = skills
+        )
+    }
+
+    @Transactional(readOnly = true)
+    fun getProjects(): List<ProjectDTO> {
+        val projects = presentationRepository.getActiveProjects()
+
+        return projects.map { ProjectDTO(it) }
+    }
+
+}
